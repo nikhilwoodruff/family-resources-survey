59e3722c98282af2a6ec78d752c77869819d02d2
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-03-28 17:02:11 +0100

    Index tables in save mode

--- family_resources_survey/process.py
+++ family_resources_survey/process.py
@@ -1,38 +1,29 @@
 import pandas as pd
 
 
-def person_id(df: pd.DataFrame) -> pd.Series:
-    return df.sernum * 1e2 + df.BENUNIT + df.PERSON
-
-
-def benunit_id(df: pd.DataFrame) -> pd.Series:
-    return df.sernum * 1e2 + df.BENUNIT
-
-
-def household_id(df: pd.DataFrame) -> pd.Series:
-    return df.sernum
 
 
 def expand_multiple_occurrences(
-    table: pd.DataFrame, id_col: str, max_count: int, count_label: str
+    table: pd.DataFrame, max_count: int, count_label: str
 ) -> pd.DataFrame:
     """Converts a table containing multiple entries per person into a table with numbered repetitions of each column for each entry, limited to a maximum, with excess entries summed into an 'other' column.
 
     Args:
         table (pd.DataFrame): The table containing multiple entries.
-        id_col (str): The identity column to join on.
         max_count (int): The maximum number of entries (includes the 'other' column).
         count_label (str): A column to fill with the number of entries.
 
     Returns:
         pd.DataFrame: The converted table.
     """
-    gb = table.groupby(id_col)
+    gb = table.groupby(table.index)
     max_number = gb.size().max()
-    df = table[[id_col]].set_index(id_col)
+    df = pd.DataFrame(index=table.index)
     for i in range(max_count - 1):
-        df = df.join(gb.nth(i), rsuffix=f"_{i + 1}")
+        df = df.join(gb.nth(i).add_suffix(f"_{i + 1}"))
     other_jobs = tuple(range(max_count - 1, max_number))
-    df = df.join(gb.nth(other_jobs).groupby("id").sum(), rsuffix="_other")
+    remaining = gb.nth(other_jobs)
+    suffix = ("_other", "")[max_count == 1]
+    df = df.join(remaining.groupby(remaining.index).sum().add_suffix(suffix))
     df[count_label] = gb.size()
     return df.fillna(0)
--- family_resources_survey/save.py
+++ family_resources_survey/save.py
@@ -2,8 +2,84 @@ import pandas as pd
 from pathlib import Path
 import os
 import shutil
+from tqdm import tqdm
 
 FRS = Path(__file__).parent
+
+
+table_entities = dict(
+    person=[
+        "accounts",
+        "adult",
+        "assets",
+        "benefits",
+        "care",
+        "child",
+        "chldcare",
+        "govpay",
+        "job",
+        "maint",
+        "oddjob",
+        "penprov",
+        "pension"
+    ],
+    benunit=[
+        "benunit",
+    ],
+    household=[
+        "endowmnt",
+        "extchild",
+        "househol",
+        "mortcont",
+        "mortgage",
+        "owner",
+        "rentcont",
+        "renter"
+    ]
+)
+
+table_to_entity = {}
+
+for entity, tables in table_entities.items():
+    for table_name in tables:
+        table_to_entity[table_name] = entity
+
+ADMIN_COLUMNS = [
+    'sernum',
+    'BENUNIT',
+    'PERSON',
+    'ISSUE', 
+    'MONTH'
+]
+
+
+def person_id(df: pd.DataFrame) -> pd.Series:
+    if "PERSON" in df.columns:
+        person_col = df.PERSON
+    elif "NEEDPER" in df.columns:
+        person_col = df.NEEDPER
+    return df.sernum * 1e2 + df.BENUNIT + person_col
+
+
+def benunit_id(df: pd.DataFrame) -> pd.Series:
+    return df.sernum * 1e2 + df.BENUNIT
+
+
+def household_id(df: pd.DataFrame) -> pd.Series:
+    return df.sernum
+
+
+def index_table(table, entity_name):
+    if entity_name == "person":
+        table["person_id"] = person_id(table)
+        table["benunit_id"] = benunit_id(table)
+        table["household_id"] = household_id(table)
+    elif entity_name == "benunit":
+        table["benunit_id"] = benunit_id(table)
+        table["household_id"] = household_id(table)
+    elif entity_name == "household":
+        table["household_id"] = household_id(table)
+    return table.set_index(f"{entity_name}_id").drop(ADMIN_COLUMNS, axis=1, errors="ignore")
 
 
 def save(folder: str, year: int, zipped: bool = True) -> None:
@@ -35,16 +111,22 @@ def save(folder: str, year: int, zipped: bool = True) -> None:
         shutil.rmtree(target_folder)
     os.makedirs(target_folder)
 
-    # Find the data type and extract.
+    # Index the data and save.
 
-    if (main_folder / "sas").exists():
-        data_folder = main_folder / "sas"
-        data_files = data_folder.glob("*.sas7bdat")
-    elif (main_folder / "tab").exists():
+    if (main_folder / "tab").exists():
         data_folder = main_folder / "tab"
-        data_files = data_folder.glob("*.tab")
-    for filepath in data_files:
-        shutil.copyfile(filepath, target_folder / filepath.name)
+        data_files = list(data_folder.glob("*.tab"))
+        task = tqdm(data_files, desc="Indexing data tables")
+        for filepath in task:
+            task.set_description(f"Indexing {filepath.name}")
+            table_name = filepath.name.replace(".tab", "")
+            if table_name in table_to_entity:
+                entity_name = table_to_entity[table_name]
+                df = pd.read_csv(filepath, delimiter="\t", low_memory=False)
+                df = index_table(df, entity_name)
+                df.to_csv(target_folder / (table_name + ".csv"))
+    else:
+        raise FileNotFoundError("Could not find the TAB files.")
 
     # Clean up tmp storage.
 
