334cbc1336e33582c09edbcd5abb1e64e8fd4c5c
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-03-28 18:24:14 +0100

    Improve consolidation function

--- family_resources_survey/process.py
+++ family_resources_survey/process.py
@@ -3,7 +3,7 @@ import pandas as pd
 from functools import reduce
 
 def consolidate(
-    table: pd.DataFrame, max_count: int = 1, count_label: str = None
+    table: pd.DataFrame, max_count: int = 1, count_label: str = None, count_all = False
 ) -> pd.DataFrame:
     """Converts a table containing multiple entries per person into a table with numbered repetitions of each column for each entry, limited to a maximum, with excess entries summed into an 'other' column.
 
@@ -11,19 +11,23 @@ def consolidate(
         table (pd.DataFrame): The table containing multiple entries.
         max_count (int): The maximum number of entries (includes the 'other' column).
         count_label (str): A column to fill with the number of entries.
+        count_all (bool): Whether to set max_count to the maximum number of occurrences for an entity.
 
     Returns:
         pd.DataFrame: The converted table.
     """
     gb = table.groupby(table.index)
     max_number = gb.size().max()
+    if count_all:
+        max_count = max_number
     df = pd.DataFrame(index=table.index)
-    for i in range(max_count - 1):
+    for i in range(max_count - 1 + (max_count == max_number)):
         df = df.join(gb.nth(i).add_suffix(f"_{i + 1}"))
-    other_jobs = tuple(range(max_count - 1, max_number))
-    remaining = gb.nth(other_jobs)
-    suffix = ("_other", "")[max_count == 1]
-    df = df.join(remaining.groupby(remaining.index).sum().add_suffix(suffix))
+    if max_count != max_number:
+        other_jobs = tuple(range(max_count - 1, max_number))
+        remaining = gb.nth(other_jobs)
+        suffix = ("_other", "")[max_count == 1]
+        df = df.join(remaining.groupby(remaining.index).sum().add_suffix(suffix))
     if count_label is not None:
         df[count_label] = gb.size()
     return df.fillna(0)
