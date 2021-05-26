c089ccdd942e9e5b4bf0e0123aeeff31db39f843
Nikhil <nikhil.woodruff@outlook.com>
2021-05-26 12:09:00 +0100

    Add uprating procedure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="family_resources_survey",
-    version="0.0.1",
+    version="1.0.0",
     description=(
         "A Python package to manage Family Resources Survey microdata."
     ),
@@ -10,7 +10,7 @@ setup(
     author="Nikhil Woodruff",
     author_email="nikhil.woodruff@outlook.com",
     packages=find_packages(),
-    install_requires=["pandas", "openpyxl", "xlrd"],
+    install_requires=["pandas", "openpyxl", "xlrd", "tqdm"],
     entry_points={
         "console_scripts": ["frs-data=family_resources_survey.main:main"],
     },
