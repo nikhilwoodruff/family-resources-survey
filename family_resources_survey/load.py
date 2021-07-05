1093d4e9019885ae6993cffb75b2e25f946536cb
Nikhil Woodruff <nikhil.woodruff@outlook.com>
2021-07-05 15:21:43 +0100

    Apply formatting

--- family_resources_survey/load.py
+++ family_resources_survey/load.py
@@ -8,11 +8,12 @@ import warnings
 ADULT_AGE_VAR = "AGE80"
 WEIGHT_VAR = "GROSS4"
 
+
 def load(
     year: int,
     table: str,
     columns: List[str] = None,
-    add_entity_ids: bool = True
+    add_entity_ids: bool = True,
 ) -> pd.DataFrame:
     year = str(year)
     data_path = FRS_path / "data" / year / "raw"
@@ -23,14 +24,17 @@ def load(
             )
             if add_entity_ids:
                 if "PERSON" in df.columns:
-                    df["person_id"] = df.sernum * 1e+2 + df.BENUNIT * 1e+1 + df.PERSON
+                    df["person_id"] = (
+                        df.sernum * 1e2 + df.BENUNIT * 1e1 + df.PERSON
+                    )
                 if "BENUNIT" in df.columns:
-                    df["benunit_id"] = df.sernum * 1e+2 + df.BENUNIT * 1e+1
+                    df["benunit_id"] = df.sernum * 1e2 + df.BENUNIT * 1e1
                 if "sernum" in df.columns:
-                    df["household_id"] = df.sernum * 1e+2
+                    df["household_id"] = df.sernum * 1e2
         return df
     else:
         raise FileNotFoundError("Could not find the data requested.")
+
 
 class Uprating:
     affected_by = {
@@ -47,30 +51,53 @@ class Uprating:
             with open(FRS_path / "uprating" / "uprating.yaml") as f:
                 self.parameters = yaml.safe_load(f)
 
-            self.population_projection_by_age = pd.read_csv(FRS_path / "uprating" / "population_projections.csv").set_index("lower_age")
-            self.population_projection = self.population_projection_by_age.sum()
+            self.population_projection_by_age = pd.read_csv(
+                FRS_path / "uprating" / "population_projections.csv"
+            ).set_index("lower_age")
+            self.population_projection = (
+                self.population_projection_by_age.sum()
+            )
 
             for variable in ("labour_income",):
                 if variable not in self.parameters:
-                    raise Exception(f"Uprating parameters do not contain {variable}")
+                    raise Exception(
+                        f"Uprating parameters do not contain {variable}"
+                    )
                 if base_year not in self.parameters[variable]:
-                    raise Exception(f"Uprating parameters do not contain the rate for {base_year} for {variable}")
+                    raise Exception(
+                        f"Uprating parameters do not contain the rate for {base_year} for {variable}"
+                    )
                 if target_year not in self.parameters[variable]:
-                    raise Exception(f"Uprating parameters do not contain the rate for {target_year} for {variable}")
-                self.multipliers[variable] = self.parameters[variable][target_year] / self.parameters[variable][base_year]
+                    raise Exception(
+                        f"Uprating parameters do not contain the rate for {target_year} for {variable}"
+                    )
+                self.multipliers[variable] = (
+                    self.parameters[variable][target_year]
+                    / self.parameters[variable][base_year]
+                )
         else:
             self.empty = True
-    
-    def uprate_adult_weight(self, adult_weight: pd.Series, age: pd.Series, ) -> pd.Series:
+
+    def uprate_adult_weight(
+        self,
+        adult_weight: pd.Series,
+        age: pd.Series,
+    ) -> pd.Series:
         lower_age = (age // 5) * 5
-        base_year_populations = self.population_projection_by_age[str(self.base_year)][lower_age].values
-        target_year_populations = self.population_projection_by_age[str(self.target_year)][lower_age].values
+        base_year_populations = self.population_projection_by_age[
+            str(self.base_year)
+        ][lower_age].values
+        target_year_populations = self.population_projection_by_age[
+            str(self.target_year)
+        ][lower_age].values
         multipliers = target_year_populations / base_year_populations
         return adult_weight * multipliers
-    
+
     def uprate_group_weight(self, group_weight: pd.Series) -> pd.Series:
         base_year_population = self.population_projection[str(self.base_year)]
-        target_year_population = self.population_projection[str(self.target_year)]
+        target_year_population = self.population_projection[
+            str(self.target_year)
+        ]
         multiplier = target_year_population / base_year_population
         return group_weight * multiplier
 
@@ -83,10 +110,15 @@ class Uprating:
                 if affected_variable in table.columns:
                     table[affected_variable] *= self.multipliers[variable]
         if ADULT_AGE_VAR in table.columns:
-            table[WEIGHT_VAR] = self.uprate_adult_weight(table[WEIGHT_VAR], table[ADULT_AGE_VAR]).values
+            table[WEIGHT_VAR] = self.uprate_adult_weight(
+                table[WEIGHT_VAR], table[ADULT_AGE_VAR]
+            ).values
         elif WEIGHT_VAR in table.columns:
-            table[WEIGHT_VAR] = self.uprate_group_weight(table[WEIGHT_VAR]).values
+            table[WEIGHT_VAR] = self.uprate_group_weight(
+                table[WEIGHT_VAR]
+            ).values
         return table
+
 
 class FRS:
     def __init__(self, year: int, add_entity_ids=True):
@@ -99,7 +131,9 @@ class FRS:
         self.variables = {}
         self.uprater = Uprating()
         if not self.data_path.exists():
-            available_years = list(map(lambda path: int(path.name), (FRS_path / "data").iterdir()))
+            available_years = list(
+                map(lambda path: int(path.name), (FRS_path / "data").iterdir())
+            )
             if len(available_years) == 0:
                 raise FileNotFoundError(f"No FRS years stored.")
             try:
@@ -107,7 +141,9 @@ class FRS:
                 self.uprater = Uprating(base_year, year)
                 self.year = base_year
             except Exception as e:
-                raise Exception(f"No data for {year} stored, and uprating failed: {e}")
+                raise Exception(
+                    f"No data for {year} stored, and uprating failed: {e}"
+                )
         if codebook_path.exists():
             with open(codebook_path, "r") as f:
                 codebook = json.load(f)
@@ -125,7 +161,9 @@ class FRS:
         if name == "description":
             return self.description
         if name not in self.tables:
-            self.tables[name] = load(self.year, name, add_entity_ids=self.add_entity_ids)
+            self.tables[name] = load(
+                self.year, name, add_entity_ids=self.add_entity_ids
+            )
         return self.uprater(self.tables[name])
 
     @property
